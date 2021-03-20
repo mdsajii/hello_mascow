2b7ed9a7fcb5732357e5fc33e302414065bcc761
mdsajii <waqirmpm@gmail.com>
2021-03-20 15:07:22 +0530

    app.js changes

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const nDate = new Date().toLocaleTimeString('en-US', {
   timeZone: 'Europe/Moscow'
 });
 
-res.send('Hello world! Time in: ' +nDate);
+res.send('Hello world! Time is: ' +nDate);
 });
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
